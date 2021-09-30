4491a091181dc7f6fd1fc820bc1ebed44c9e8d1b
RicardoAgra <rmagra@isq.pt>
2021-09-30 18:43:57 +0100

    feat: encapsulation

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # python-design-patterns
 
 ## Encapsulation
-	Encapsulation is the devision of a program into disctinct components which have limited interaction.
-	Advantages of Encapsulation:
-		- Easier to test: Each component can be tested seperatly.
-		- Modularization: New functionality can be developed based on the existing code base,
-						wihtout the need to right extra code or duplicated code.
+
+Encapsulation is the devision of a program into disctinct components which have limited interaction.
+Advantages of Encapsulation:
+
+-   Easier to test: Each component can be tested seperatly.
+
+-   Modularization: New functionality can be developed based on the existing code base,
+    wihtout the need to right extra code or duplicated code.
+
+## Abstraction
+
+Abstration means we should hide unnecessary details in our classes and objects in order to reduce complexity.
 
 [Tuturial](https://www.youtube.com/watch?v=NU_1StN5Tkk)
--- encapsulation.py
+++ encapsulation.py
@@ -1,6 +1,7 @@
 from abc import abstractmethod
 from dataclasses import dataclass
 
+
 class InformalUserInterface:
 	@abstractmethod
 	def say_hello(self) -> str:
@@ -14,18 +15,33 @@ class InformalUserInterface:
 	def set_age(self, new_age: int) -> None:
 		pass
 
+
 @dataclass
 class User(InformalUserInterface):
 	"""Class for registering an user"""
 	first_name: str
 	last_name: str
-	age: int = 0
+	_age: int = 0
 
+	"""This methods internal procedures can be changed without affecting it's dependencies"""
 	def say_hello(self) -> str:
 		return f"Hello, my name is {self.first_name} {self.last_name}, I am pleased to meet you!"
 
 	def get_age(self) -> int:
-		return self.age
+		return self._age
 
 	def set_age(self, new_age: int) -> None:
-		self.age = new_age
+		if new_age < 0:
+			return
+		self._age = new_age
+
+
+
+if __name__ == "__main__":
+	print('Encapsulation \n')
+
+	new_user = User("Ricardo", "Agra")
+
+	print(new_user.say_hello())
+
+	print(new_user.get_age())
